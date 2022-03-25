983be6c04ad505e6c357af4283dc31765196bcd5
Ivan <keranamv@gmail.com>
2022-03-25 12:44:27 +0100

    Initial Commit

---
+++ HalfModalSwiftUI/HalfModalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfModalSwiftUIApp.swift
+//  HalfModalSwiftUI
+//
+//  Created by Ivan Voloshchuk on 25/03/22.
+//
+
+import SwiftUI
+
+@main
+struct HalfModalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
